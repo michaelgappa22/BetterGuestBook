7e2ef2b95d14fde459f11e54f870637f0931c395
michaelgappa22 <m.gappa2293@gmail.com>
2023-05-06 16:30:38 -0400

    Organized Methods
    Organized project to separate Methods with Program file.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using GuestLibrary.Models;
+using GuestLibrary.Methods;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +19,7 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             GetGuestInformation();
-            AnnounceGuests();
+            GuestsMethods.AnnounceGuests(guests);
 
             Console.ReadLine();
         }
@@ -30,57 +31,18 @@ namespace ConsoleUI
             {
                 GuestModel guest = new GuestModel();
 
-                guest.FirstName = RetrieveDataFromGuests("What is your First name: ");
-                guest.LastName = RetrieveDataFromGuests("What is your Last name: ");
-                guest.Email = new MailAddress(RetrieveDataFromGuests("What is your Email Address: "));
-                guest.Birthday = RetrieveBirthdayFromGuest("When is your Birthday (month/day/year): ");
-                guest.MessageToHost = RetrieveDataFromGuests("What is your Message: ");
-                moreGuests = RetrieveDataFromGuests("Will there be more guests (yes/no): ");
+                guest.FirstName = GuestsMethods.RetrieveDataFromGuests("What is your First name: ");
+                guest.LastName = GuestsMethods.RetrieveDataFromGuests("What is your Last name: ");
+                guest.Email = new MailAddress(GuestsMethods.RetrieveDataFromGuests("What is your Email Address: "));
+                guest.Birthday = GuestsMethods.RetrieveBirthdayFromGuest("When is your Birthday (month/day/year): ");
+                guest.MessageToHost = GuestsMethods.RetrieveDataFromGuests("What is your Message: ");
+                moreGuests = GuestsMethods.RetrieveDataFromGuests("Will there be more guests (yes/no): ");
                 
                 Console.Clear();
 
                 guests.Add(guest);
 
             } while (moreGuests.ToLower() != "no");
-        }
-
-        private static void AnnounceGuests()
-        {
-            foreach (GuestModel guest in guests)
-            {
-                Console.WriteLine(guest.GuestAnnouncement);
-                SpecialSeating(guest);
-            }
-
-        }
-        private static void SpecialSeating(GuestModel guest)
-        {
-            // converts TimeSpan object from DateTime.Now.Date - guest.Birthday to int and divides the result with 365 to find age of guest.
-            int age = Convert.ToInt32(((DateTime.Now.Date - guest.Birthday).TotalDays / 365));
-            if (age > 65)
-            {
-                Console.WriteLine($"{guest.FirstName}, please see Tim for escort.");
-            }
-        }
-
-        private static string RetrieveDataFromGuests(string message)
-        {
-            Console.WriteLine(message);
-            string data = Console.ReadLine();
-            return data;
-        }
-
-        private static DateTime RetrieveBirthdayFromGuest(string message)
-        {
-            Console.WriteLine(message);
-            bool isValid = DateTime.TryParse(Console.ReadLine(), out DateTime birthday);
-            while (isValid != true)
-            {
-                Console.WriteLine("That is not a valid format. Your format should be month/day/year. Please try again");
-                Console.WriteLine(message);
-                isValid = DateTime.TryParse(Console.ReadLine(), out birthday);
-            }
-            return birthday;
         }
 
     }
---
+++ GuestLibrary/Methods/GuestsMethods.cs
@@ -0,0 +1,51 @@
+﻿using GuestLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestLibrary.Methods
+{
+    public class GuestsMethods
+    {
+        public static void AnnounceGuests(List<GuestModel> guests)
+        {
+            foreach (GuestModel guest in guests)
+            {
+                Console.WriteLine(guest.GuestAnnouncement);
+                SpecialSeating(guest);
+            }
+
+        }
+        public static void SpecialSeating(GuestModel guest)
+        {
+            // converts TimeSpan object from DateTime.Now.Date - guest.Birthday to int and divides the result with 365 to find age of guest.
+            int age = Convert.ToInt32(((DateTime.Now.Date - guest.Birthday).TotalDays / 365));
+            if (age > 65)
+            {
+                Console.WriteLine($"{guest.FirstName}, please see Tim for escort.");
+            }
+        }
+
+        public static string RetrieveDataFromGuests(string message)
+        {
+            Console.WriteLine(message);
+            string data = Console.ReadLine();
+            return data;
+        }
+
+        public static DateTime RetrieveBirthdayFromGuest(string message)
+        {
+            Console.WriteLine(message);
+            bool isValid = DateTime.TryParse(Console.ReadLine(), out DateTime birthday);
+            while (isValid != true)
+            {
+                Console.WriteLine("That is not a valid format. Your format should be month/day/year. Please try again");
+                Console.WriteLine(message);
+                isValid = DateTime.TryParse(Console.ReadLine(), out birthday);
+            }
+            return birthday;
+        }
+    }
+}
